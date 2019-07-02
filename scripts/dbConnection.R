d42608605c9b107da3f79d13abbfcefb6c0bd5bd
Elisabeth Pachl <pachl@cip.ifi.lmu.de>
2019-07-02 16:20:07 +0200

    Update dbConnection.R

--- scripts/dbConnection.R
+++ scripts/dbConnection.R
@@ -3,14 +3,14 @@ library(RMySQL)
 
 #connect to database
 drv <- dbDriver("MySQL")
-con = dbConnect(drv, user = 'root', password= "Pfiffi.kus14", dbname = 'test',host='localhost')
+con = dbConnect(drv, user = 'root', password= "Pfiffi.kus14", dbname = 'sponge',host='localhost')
 
 #list all tables in database
 dbListTables(con)
 
 #inserting data into mysql table
 #test_df <- data.frame("a" = c(1:10), "b" = letters[1:10])
-dbWriteTable(con, name = "letter", value = test_df, overwrite = FALSE, append = TRUE, row.names = FALSE)
+dbWriteTable(con, name = "dataset", value = cancer, overwrite = FALSE, append = TRUE, row.names = FALSE)
 
 #disconnect from db
 dbDisconnect(con)

56a63b1dcf033e06b900001bde0517fa54767ff5
IceQueen1996 <50208793+IceQueen1996@users.noreply.github.com>
2019-12-18 10:17:26 +0100

    add new endpoint: getGeneInformation

--- externalInformation.py
+++ externalInformation.py
@@ -41,3 +41,44 @@ def getAutocomplete(searchString):
             return models.GeneSchemaSymbol(many=True).dump(data).data
         else:
             abort(404, "No gene symbol found for the given String")
+
+def getGeneInformation(ensg_number = None, gene_symbol = None):
+    """
+    :param ensg_number:
+    :param gene_symbol:
+    :return: Available information for given gene identifier
+    """
+
+    # test if any of the two identification possibilites is given
+    if ensg_number is None and gene_symbol is None:
+        abort(404, "One of the two possible identification numbers must be provided")
+
+    # test if not both identification possibilites are given
+    if ensg_number is not None and gene_symbol is not None:
+        abort(404,
+              "More than one identifikation paramter is given. Please choose one out of (ensg number, gene symbol)")
+
+    if ensg_number is not None:
+        data = models.Gene.query \
+            .filter(models.Gene.ensg_number.in_(ensg_number)) \
+            .all()
+
+        if len(data) > 0:
+            return models.GeneSchema(many=True).dump(data).data
+        else:
+            abort(404, "No gene found with: " + ensg_number)
+
+    elif gene_symbol is not None:
+        data = models.Gene.query \
+            .filter(models.Gene.gene_symbol.in_(gene_symbol)) \
+            .all()
+
+        if len(data) > 0:
+            return models.GeneSchema(many=True).dump(data).data
+        else:
+            abort(404, "No gene found with: " + gene_symbol)
+
+
+
+
+

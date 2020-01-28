c138e8765b265ec063ce8c9a35fa98d793c6a434
IceQueen1996 <50208793+IceQueen1996@users.noreply.github.com>
2020-01-28 16:37:26 +0100

    delete debug sql statements

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ app = connex_app.app
 CORS(app)
 
 # Configure the SQLAlchemy part of the app instance
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SPONGE_DB_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 

3886baa7f80158ccee6e754100dc1b34ee052a35
IceQueen1996 <50208793+IceQueen1996@users.noreply.github.com>
2020-02-06 16:59:46 +0100

    update home function

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ connex_app.add_api("swagger.yml")
 # create a URL route in our application for "/"
 @connex_app.route("/")
 def home():
-    return None
+    return "SPONGE-API. For furhter information please use the URL + /ui."
 
 if __name__ == "__main__":
     connex_app.run(debug=False)

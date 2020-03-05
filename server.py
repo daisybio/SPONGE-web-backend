38afbc31d9e60c6a91660c3edab3440321b1ec48
IceQueen1996 <50208793+IceQueen1996@users.noreply.github.com>
2020-03-05 15:08:40 +0100

    Update server.py

--- server.py
+++ server.py
@@ -13,6 +13,10 @@ connex_app = config.connex_app
 # Read the swagger.yml file to configure the endpoints
 connex_app.add_api("swagger.yml")
 
+@connex_app.route("/")
+def home():
+    return None
+
 cli = sys.modules['flask.cli']
 cli.show_server_banner = lambda *x: None
 

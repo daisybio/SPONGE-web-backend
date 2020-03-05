41e6c87a188655962d9e08de7f6d482fc2e13075
IceQueen1996 <50208793+IceQueen1996@users.noreply.github.com>
2020-03-05 15:24:18 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ connex_app = config.connex_app
 # Read the swagger.yml file to configure the endpoints
 connex_app.add_api("swagger.yml")
 
+# create a URL route in our application for "/"
 @connex_app.route("/")
 def home():
     return None
